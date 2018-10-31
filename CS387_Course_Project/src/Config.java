0c6e68b2b63713b942e423e5bbbb26d937aa20d0
syamantak <syamantakk@cse.iitb.ac.in>
2018-10-31 20:11:14 +0530

    Creating the project skeleton

---
+++ CS387_Course_Project/src/Config.java
@@ -0,0 +1,9 @@
+
+/**
+ * Edit to provide your database credentials
+ */
+public class Config {
+	public static final String url = "jdbc:postgresql://localhost:5432/postgres";
+	public static final String user = "syamantak";
+	public static final String password = "";
+}
