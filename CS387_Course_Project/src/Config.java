30b013264787ba641be67caf901c6518df5c7ec6
Kartik14 <kartikkhandelwal1998@gmail.com>
2018-11-27 13:31:46 +0530

    config

--- CS387_Course_Project/src/Config.java
+++ CS387_Course_Project/src/Config.java
@@ -3,7 +3,7 @@
  * Edit to provide your database credentials
  */
 public class Config {
-	public static final String url = "jdbc:postgresql://localhost:5432/postgres";
-	public static final String user = "syamantak";
+	public static final String url = "jdbc:postgresql://localhost:5258/postgres";
+	public static final String user = "kartik";
 	public static final String password = "";
 }
